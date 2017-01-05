18dceed2073ef3adb37b6dfec9fd49a33066ec80
Mike McGuire <MoonshotMike@gmail.com>
2017-01-05 13:20:43 -0700

    stuck on lecture 22

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message: req.params.msg});
-});
+router.post('/', function (req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Mike',
+        lastName: 'McGuire',
+        password: 'Codewizard',
+        email: email
+    });
 
-router.post('/message', function (req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+    user.save();
+
+    res.redirect('/');
 });
 
 module.exports = router;
